47ff54a53ec79d727ba0d77d969784a1337dd6d9
Stephen Fox <stephenfox995@hotmail.com>
2017-04-14 16:12:21 +0100

    Updated rest_urls

--- tracker/rest_urls.py
+++ tracker/rest_urls.py
@@ -3,7 +3,7 @@ from tracker import rest_views
 
 
 urlpatterns = [
-    url(r'activity/', rest_views.ActivityRetrieveAPI.as_view(), name="activity"),
+    url(r'activity', rest_views.ActivityRetrieveAPI.as_view(), name="activity"),
     url(r'^token-auth/$', rest_views.obtain_auth_token, name='obtain-token-auth'),
     url(r'^register', rest_views.register, name="register"),
 ]
